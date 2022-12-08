2db5360e304d6adbbc8d8b3a548d26e7fcd6555d
정준영 <jeongjy@mrblue.com>
2022-12-08 16:35:26 +0900

    feat: ch2 문제 해결 추가

--- src/main/java/com/example/design_pattern/ch2_adapter/q2/FileProperties.java
+++ src/main/java/com/example/design_pattern/ch2_adapter/q2/FileProperties.java
@@ -1,1 +1,32 @@
 package com.example.design_pattern.ch2_adapter.q2;// FileProperties 클래스를 구현해 보세요.
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Properties;
+
+public class FileProperties implements FileIO {
+
+    Properties properties = new Properties();
+
+    public void readFromFile(String filename) throws IOException {
+      Reader reader = new FileReader(filename);
+      properties.load(reader);
+    }
+
+    public void writeToFile(String filename) throws IOException {
+        // properties의 내용을 파일에 저장한다.
+      properties.store(new FileWriter(filename), "written by FileProperties");
+    }
+
+    public void setValue(String key, String value) {
+        // properties에 key와 value를 설정한다.
+      properties.setProperty(key, value);
+    }
+
+    public String getValue(String key) {
+        // properties에서 key에 해당하는 값을 얻는다.
+      return properties.getProperty(key);
+    }
+}
--- src/main/java/com/example/design_pattern/ch2_adapter/q2/Main.java
+++ src/main/java/com/example/design_pattern/ch2_adapter/q2/Main.java
@@ -4,16 +4,18 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-//        FileIO f = new FileProperties();
-//        try {
-//            f.readFromFile("file.txt");
-//            f.setValue("width", "1024");
-//            f.setValue("height", "512");
-//            f.setValue("depth", "32");
-//            f.writeToFile("newfile.txt");
-//            System.out.println("newfile.txt is created.");
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+        FileIO f = new FileProperties();
+        String path = System.getProperty("user.dir") + "/src/main/java/com/example/design_pattern/ch2_adapter/q2/";
+        System.out.println("1111 : " + path);
+        try {
+            f.readFromFile(path + "file.txt");
+            f.setValue("width", "1024");
+            f.setValue("height", "512");
+            f.setValue("depth", "32");
+            f.writeToFile(path + "newfile.txt");
+            System.out.println("newfile.txt is created.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
